f720d1a54c1aca62eb756a8d2a833f8fd90b5831
akiramatsumoto <akira.matsumoto@hotmail.co.jp>
2015-08-21 20:22:40 +0900

    はじめてのクローン
    か～ら～のプッシュ！！！

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/mitchellh/go-ps"
 )
 
 func main() {
-	procs, _ := ps.Processes()
-	for i, c := range procs {
-		fmt.Println(i, c)
-	}
+	fmt.Println("Hello GitHub!!!")
 }
